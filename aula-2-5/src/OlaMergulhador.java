ff2737e26355ba669531c77ec0099dcea390b821
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-28 14:43:03 -0300

    2.5. Conhecendo e usando convenções de código

---
+++ aula-2-5/src/OlaMergulhador.java
@@ -0,0 +1,11 @@
+//2.5. Conhecendo e usando convenções de código
+
+public class OlaMergulhador {
+
+  public static void main(String[] args) {
+    System.out.println("Olá, mergulhador!");
+  }
+
+}
+
+
