940c5b09a073dc1a8c1e7848c896a490c048b498
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-28 14:31:07 -0300

    2.4. Escrevendo comentários no código

---
+++ aula-2-4/src/OlaMergulhador.java
@@ -0,0 +1,15 @@
+//Você pode comentar uma linha inteira
+public class OlaMergulhador {
+  /*
+  Comentário de bloco
+  Método principal do programa.
+  Ao executar o programa, esse método será chamado automaticamente.
+   */
+
+  public static void main(String[] args) {
+    //Imprime a mensagem na saída pddrão
+    System.out.println("Olá, mergulhador!");
+  }
+
+
+}
