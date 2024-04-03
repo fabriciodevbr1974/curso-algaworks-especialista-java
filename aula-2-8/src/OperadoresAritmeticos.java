7fcdce5c67516fc6ed419ac871f0caa212288a1c
Fabrício Guimarães de Lima <br.dev.fabricio@gmail.com>
2024-04-02 22:03:50 -0300

    2.8. Operadores aritméticos

---
+++ aula-2-8/src/OperadoresAritmeticos.java
@@ -0,0 +1,25 @@
+public class OperadoresAritmeticos {
+
+  public static void main(String[] args) {
+
+    int minhaIdade = 40;
+    int suaIdade = 25;
+
+    int totalIdade = minhaIdade + suaIdade;
+    System.out.println("Soma das idades: " + totalIdade);
+
+    int diferencaIdades = minhaIdade - suaIdade;
+    System.out.println("Diferença das idades: " + diferencaIdades);
+
+    int dobroDaIdade = 2 * suaIdade;
+    System.out.println("Dobro da idade: " + dobroDaIdade);
+
+    int metadeDaIdade = suaIdade / 2;
+    System.out.println("Metade da idade: " + metadeDaIdade);
+
+    int restaDivisao = 7 % 2;
+    System.out.println("Resto da divisão: " + restaDivisao);
+
+  }
+
+}
