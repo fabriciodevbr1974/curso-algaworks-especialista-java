5445c7fba87ac88fd36d5f8b313d64b1bb80eb5a
Fabrício Guimarães de Lima <br.dev.fabricio@gmail.com>
2024-04-02 22:27:52 -0300

    2.11. Operadores de incremento e decremento

---
+++ aula-2-11/src/OperadorDecremento.java
@@ -0,0 +1,15 @@
+public class OperadorDecremento {
+
+  public static void main(String[] args) {
+    int totalDownloads = 10;
+
+//    totalDownloads = totalDownloads + 1;
+//    totalDownloads -= 1;
+//    totalDownloads--;//pós fixada
+
+//    int novoTotalDownloads = totalDownloads--;
+    int novoTotalDownloads = --totalDownloads;//pré fixado
+    System.out.println("Total de downloads: " + totalDownloads);
+    System.out.println("Total de downloads: " + novoTotalDownloads);
+  }
+}
