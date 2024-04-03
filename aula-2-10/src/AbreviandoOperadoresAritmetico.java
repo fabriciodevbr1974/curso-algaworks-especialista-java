be9ffa34a0c08299d895605adcb4f0cc55eb1468
Fabrício Guimarães de Lima <br.dev.fabricio@gmail.com>
2024-04-02 22:14:32 -0300

    2.10. Abreviando operadores aritméticos

---
+++ aula-2-10/src/AbreviandoOperadoresAritmetico.java
@@ -0,0 +1,18 @@
+public class AbreviandoOperadoresAritmetico {
+
+  public static void main(String[] args) {
+    int quantidadeProdutos = 10;
+//    quantidadeProdutos = quantidadeProdutos + 3;
+    quantidadeProdutos += 3;
+    System.out.println(quantidadeProdutos);
+    quantidadeProdutos -= 3;
+    System.out.println(quantidadeProdutos);
+    quantidadeProdutos *= 3;
+    System.out.println(quantidadeProdutos);
+    quantidadeProdutos /= 3;
+    System.out.println(quantidadeProdutos);
+    quantidadeProdutos %= 3;
+    System.out.println(quantidadeProdutos);
+
+  }
+}
