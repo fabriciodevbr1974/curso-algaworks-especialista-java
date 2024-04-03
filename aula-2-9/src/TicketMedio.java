1988e1d8d5e33e6b3829f1adf3dd93902c53b143
Fabrício Guimarães de Lima <br.dev.fabricio@gmail.com>
2024-04-02 22:08:06 -0300

    2.9. Desafio: variáveis e operadores aritméticos

---
+++ aula-2-9/src/TicketMedio.java
@@ -0,0 +1,12 @@
+public class TicketMedio {
+
+  public static void main(String[] args) {
+    int venda1 = 20;
+    int venda2 = 30;
+    int venda3 = 100;
+
+    int media = (venda1 + venda2 + venda3) / 3;
+
+    System.out.println("Ticket Médio: " + media);
+  }
+}
