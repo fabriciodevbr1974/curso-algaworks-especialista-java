f34b58bfbe7ad1420a18d0d290489b79fe995083
Fabrício Guimarães de Lima <br.dev.fabricio@gmail.com>
2024-04-02 22:43:45 -0300

    2.12. Tipos primitivos: boolean, char, byte e short

---
+++ aula-2-12/src/ExemploByte.java
@@ -0,0 +1,13 @@
+public class ExemploByte {
+
+  public static void main(String[] args) {
+
+    byte idade = 127;
+//    byte idade2 = 128;
+
+    short quantidadeEstoque = 32767;
+
+    System.out.println(idade);
+
+  }
+}
---
+++ aula-2-12/src/ExemploChar.java
@@ -0,0 +1,11 @@
+public class ExemploChar {
+
+  public static void main(String[] args) {
+
+    char inicialDoNome = 'F';
+    char tipoCliente = '2';
+    char simbolo = '@';
+    System.out.println(inicialDoNome);
+
+  }
+}
